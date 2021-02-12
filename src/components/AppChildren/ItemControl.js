f6cc9a104f77edd2fc3ea31e26c6a15786047ba1
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 01:50:40 -0500

    refactor button to local state

--- src/components/AppChildren/ItemControl.js
+++ src/components/AppChildren/ItemControl.js
@@ -10,21 +10,31 @@ class ItemControl extends React.Component {
       formVisibleOnPage: false
     };
   }
+  //this is a method that has to be called on an instance of ItemControl class
+  handleClick = () => {
+    //setState takes an object as an argument
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  };
   render() {
     // we havn't determined which component should be visible
     let currentlyVisibleState = null;
-    let addItemButton = null;
+    let buttonText = null;
     //access any property in this state
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewItemForm />;
+      buttonText = "Return to Item List";
     } else {
       currentlyVisibleState = <ItemList />;
-      addItemButton = <button onClick={this.handleClick}>Add Item</button>;
+      //this is a local state for the refactoring of buttons(and the one in return)
+      buttonText = "Add Item";
     }
     return (
       <>
       {currentlyVisibleState}
-      {addItemButton}
+      
+      <button onClick={this.handleClick}>{buttonText}</button>
       </>
     );
   }
